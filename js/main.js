60a8c6baaf7ae9c2236daf41921152f41d920178
Maksim Burakou <maxashtet@gmail.com>
2021-05-08 15:44:12 +0300

    feat: add feedback and our-work sliders

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+const ourServicesSlider = new Swiper('.our-services-slider', {
+  slidesPerView: 1,
+  
+})
+
+const feedbackSwiper = new Swiper('.feedback-slider', {
+  centeredSlides: true,
+  slidesPerView: 2.15,
+  spaceBetween: 99,
+  simulateTouch: false,
+  loop: true,
+  slideActiveClass: 'swiper-slide-active',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
 const ourWorkSwiper = new Swiper('.our-work-slider', {
   centeredSlides: true,
   slidesPerView: 2.15,
-  spaceBetween: 65,
+  spaceBetween: 99,
   simulateTouch: false,
   loop: true,
   slideActiveClass: 'swiper-slide-active',
