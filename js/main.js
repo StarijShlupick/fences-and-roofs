8477766c6326eeb8e0f7f22414e27fefc0535d8e
Maksim Burakou <maxashtet@gmail.com>
2021-05-10 23:35:49 +0300

    feat: work-cases

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ const ourServicesSlider = new Swiper('.our-services-slider', {
   slidesPerView: 1,
   spaceBetween: 0,
   simulateTouch: false,
-  // hashNavigation: {
-  //   watchState: true
-  // },
   effect: 'fade',
   fadeEffect: {
     crossFade: true
@@ -20,7 +17,6 @@ const ourServicesSlider = new Swiper('.our-services-slider', {
       },
   }
 })
-
 const ourServicesFencesSlider = new Swiper('.our-services__fences-slider', {
   slidesPerView: 4,
   spaceBetween: 67,
@@ -35,6 +31,49 @@ const ourServicesFencesSlider = new Swiper('.our-services__fences-slider', {
 const ourServicesRoofsSlider = new Swiper('.our-services__roofs-slider', {
   slidesPerView: 4,
   spaceBetween: 67,
+  simulateTouch: false,
+  nested: true,
+  slideActiveClass: 'false',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
+
+const workCasesSlider = new Swiper('.work-cases-slider', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  simulateTouch: false,
+  hashNavigation: {
+    watchState: true
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  slideActiveClass: 'false',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (bulletButtons[index]) + '</span>';
+      },
+  }
+})
+const workCasesFencesSlider = new Swiper('.work-cases__fences-slider', {
+  slidesPerView: 5,
+  spaceBetween: 36,
+  simulateTouch: false,
+  nested: true,
+  slideActiveClass: 'false',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
+const workCasesRoofsSlider = new Swiper('.work-cases__roofs-slider', {
+  slidesPerView: 5,
+  spaceBetween: 36,
   simulateTouch: false,
   nested: true,
   slideActiveClass: 'false',
@@ -69,3 +108,6 @@ const ourWorkSwiper = new Swiper('.our-work-slider', {
     prevEl: '.swiper-button-prev',
   }
 });
+
+const toScroll = idElement => 
+document.getElementById(idElement).scrollIntoView({behavior: 'smooth'});
