09dcd539ddf4a229478827199f6e60eb4e780476
Maksim Burakou <maxashtet@gmail.com>
2021-05-12 18:07:50 +0300

    feat: redesign

--- js/main.js
+++ js/main.js
@@ -1,44 +1,4 @@
-const bulletButtons = ['Заборы', 'Крыши']
-
-const ourServicesSlider = new Swiper('.our-services-slider', {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  simulateTouch: false,
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
-  slideActiveClass: 'false',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (bulletButtons[index]) + '</span>';
-      },
-  }
-})
-const ourServicesFencesSlider = new Swiper('.our-services__fences-slider', {
-  slidesPerView: 4,
-  spaceBetween: 67,
-  simulateTouch: false,
-  nested: true,
-  slideActiveClass: 'false',
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-})
-const ourServicesRoofsSlider = new Swiper('.our-services__roofs-slider', {
-  slidesPerView: 4,
-  spaceBetween: 67,
-  simulateTouch: false,
-  nested: true,
-  slideActiveClass: 'false',
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-})
+const bulletButtons = ['Заборы', 'Ворота и калитки']
 
 const workCasesSlider = new Swiper('.work-cases-slider', {
   slidesPerView: 1,
