2bd1d5db8864a6e79df3715b07d9220b3a62555b
Maksim Burakou <maxashtet@gmail.com>
2021-05-13 13:08:42 +0300

    refact: pagination and modal

--- js/main.js
+++ js/main.js
@@ -1,41 +1,42 @@
-const bulletButtons = ['Заборы', 'Ворота и калитки']
+// const bulletButtons = ['Заборы', 'Ворота и калитки']
 
-const workCasesSlider = new Swiper('.work-cases-slider', {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  simulateTouch: false,
-  hashNavigation: {
-    watchState: true
-  },
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
-  slideActiveClass: 'false',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (bulletButtons[index]) + '</span>';
-      },
-  }
-})
+// const workCasesSlider = new Swiper('.work-cases-slider', {
+//   slidesPerView: 1,
+//   spaceBetween: 0,
+//   noSwiping: true,
+//   simulateTouch: false,
+//   preventClicks: false,
+//   preventClicksPropagation: false,
+//   hashNavigation: {
+//     watchState: true
+//   },
+//   effect: 'fade',
+//   fadeEffect: {
+//     crossFade: true
+//   },
+//   slideActiveClass: 'false',
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//       renderBullet: function (index, className) {
+//         return '<span class="' + className + '">' + (bulletButtons[index]) + '</span>';
+//       },
+//   }
+// })
 const workCasesFencesSlider = new Swiper('.work-cases__fences-slider', {
   slidesPerView: 5,
   spaceBetween: 36,
   simulateTouch: false,
-  nested: true,
   slideActiveClass: 'false',
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   }
 })
-const workCasesRoofsSlider = new Swiper('.work-cases__roofs-slider', {
+const workCasesGatesSlider = new Swiper('.work-cases__gates-slider', {
   slidesPerView: 5,
   spaceBetween: 36,
   simulateTouch: false,
-  nested: true,
   slideActiveClass: 'false',
   navigation: {
     nextEl: '.swiper-button-next',
@@ -69,5 +70,81 @@ const ourWorkSwiper = new Swiper('.our-work-slider', {
   }
 });
 
-const toScroll = idElement => 
-document.getElementById(idElement).scrollIntoView({behavior: 'smooth'});
+const toScroll = idElement =>
+  document.getElementById(idElement).scrollIntoView({ behavior: 'smooth' });
+
+
+const tabNav = document.querySelectorAll('.nav__item');
+const tabContent = document.querySelectorAll('.tab');
+let tabName;
+
+function selectTabNav() {
+  tabNav.forEach(item => {
+    item.classList.remove('--active');
+  });
+  this.classList.add('--active');
+  tabName = this.getAttribute('data-tab-name');
+  selectTabContent(tabName);
+}
+
+function selectTabContent(tabName) {
+  tabContent.forEach(item => {
+    item.classList.contains(tabName) ? 
+    item.classList.add('--active') : 
+    item.classList.remove('--active');
+    switch (tabName){
+      case 'fences':{
+        workCasesFencesSlider.update();
+        break;
+      }
+      case 'gates':{
+        workCasesGatesSlider.update();
+        break;
+      }
+    }
+  })
+}
+
+tabNav.forEach(item => {
+  item.addEventListener('click', selectTabNav)
+})
+
+
+
+const openModalFencesSlides = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalFencesSlides.forEach(slide => {
+  slide.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const modal = document.querySelector(slide.dataset.modalTarget);
+    openModal(modal);
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.--active');
+  modals.forEach(modal => {
+    closeModal(modal);
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('--active');
+  overlay.classList.add('--active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('--active');
+  overlay.classList.remove('--active');
+}
